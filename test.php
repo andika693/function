20152ce83285c51a7081c83d066240f17738fe8f
andika693 <andikaxynx@gmail.com>
2024-11-28 13:26:53 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+
+  //function yg tdk mengembalikan nilai
+    function tampilkan nama() {
+        echo "Hallo, nama saya andika";
+    }
+
+    tampilkanNama();
+
+    echo "<br>";
+
+    function penjumlahan($nilai1,$nilai2){
+        $hasil = $nilai1 + $nilai2;
+        return $hasil;
+    }
+
+    echo (penjumlahan(5123,567));
+
+    echo "<br>";
+
+    function perkalian($angka1,$angka2){
+        $jumlah = $angka1 + $angka2
+        return $jumlah;
+    }
+
+    echo (perkalian(44,77));
